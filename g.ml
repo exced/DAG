194fd59a8c655bd9dec2d5f203c48163f814f96c
thomas <thomas.barras26@gmail.com>
2016-06-06 18:42:36 +0200

    heuristique done

--- g.ml
+++ g.ml
@@ -1,53 +1,6 @@
 open Graph.Pack.Digraph;;
 open DAG;;
 
-(* exemples de graphes *) 
-(* le graphe de la page 5 du sujet de projet *)
-let g1 = create ();;
-let ag1=V.create ("a",2);;
-let bg1=V.create ("b",1);;
-let cg1=V.create ("c",1);;
-let dg1=V.create ("d",3);;
-let eg1=V.create ("e",2);;
-let fg1=V.create ("f",1);;
-add_vertex g1 ag1;;
-add_vertex g1 bg1;;
-add_vertex g1 cg1;;
-add_vertex g1 dg1;;
-add_vertex g1 eg1;;
-add_vertex g1 fg1;;
-add_edge g1 ag1 dg1;;
-add_edge g1 bg1 dg1;;
-add_edge g1 bg1 eg1;;
-add_edge g1 cg1 eg1;;
-add_edge g1 dg1 fg1;;
-add_edge g1 eg1 fg1;;
-
-
-
-let g2 = create ();;
-let ag2=V.create ("a",0);;
-let bg2=V.create ("b",1);;
-let cg2=V.create ("c",2);;
-let dg2=V.create ("d",3);;
-let eg2=V.create ("e",4);;
-let fg2=V.create ("f",5);;
-let gg2=V.create ("g",6);;
-add_vertex g2 ag2;;
-add_vertex g2 bg2;;
-add_vertex g2 cg2;;
-add_vertex g2 dg2;;
-add_vertex g2 eg2;;
-add_vertex g2 fg2;;
-add_vertex g2 gg2;;
-add_edge g2 ag2 dg2;;
-add_edge g2 bg2 eg2;;
-add_edge g2 cg2 fg2;;
-add_edge g2 dg2 eg2;;
-add_edge g2 eg2 fg2;;
-add_edge g2 fg2 gg2;;
-
-
 
 (* ------------------------ Affichage ------------------------ *)
 let printSommets g = fold_vertex (fun v qt -> Format.printf "%s " (DAG.Display.vertex_name v)) g ();;
@@ -56,6 +9,7 @@ let printAretes g = fold_edges (fun v1 v2 qt-> Format.printf "- %s %s -" (DAG.Di
 let printListeSommets l = List.fold_right (fun v qt -> Format.printf "%s " (DAG.Display.vertex_name v)) l ();;
 let printListeListeSommets l = List.map (fun v -> begin Format.printf "[ "; (printListeSommets v); Format.printf " ] " end ) l;;
 let printMarkfromList l = List.fold_right (fun v qt -> Format.printf "%i " (Mark.get v)) l ();; 
+
 
 (* ------------------------ sansDep ------------------------ *)
 (* sans dependances
@@ -67,9 +21,6 @@ let sansDep g =
 	fold_vertex (fun t q -> if ((pred g t) = [] ) then t::q else q) g []
 ;;
 
-(* tests *)
-let testSansDepg1 = sansDep g1;;
-let ltestSansDepg1 = printListeSommets testSansDepg1;; 
 
 (* ------------------------ Manipulation listes ------------------------ *)
 (* entrees: 	
@@ -92,36 +43,6 @@ let listInclude l1 l2 =
 	List.fold_right (fun vt vq -> (varInclude vt l2) && vq) l1 true;
 ;;
 
-(* prise en main de la fonction List.sort *) 
-let sortByUe l =
-	List.sort (fun a b -> if a < b then -1 else (if a > b then 1 else 0)) l
-;;
-
-(* tests *)
-let l1 = [1;2;3];;
-let l2 = [2;3];;
-let l3 = [4;2;3];;
-let l4 = [1;2;5;6];;
-let l5 = [7;2;5;4];;
-varInclude 1 l1;;
-listInclude l2 l1;;
-listInclude l3 l4;;
-let lsorted = sortByUe l5;;
-
-(* ------------------------ Manipulation Graphe ------------------------ *)
-(* entrees: 	
-   	- vertex
-	- g
-   	sorties:
-   	- bool 
-	pre:   
-*)
-
-let sortByMarkUe l =
-	List.sort (fun a b -> if (Mark.get a) > (Mark.get b) then -1 else (if (Mark.get a) < (Mark.get b) then 1 else 0)) l
-;;
-
-
 
 (* ------------------------ tri_topologique ------------------------ *)
 (* entrees: 
@@ -133,6 +54,7 @@ let sortByMarkUe l =
    
 val tri_topologique : DAG.t -> DAG.vertex list
 *)
+
 let tri_topologique g =
 	let rec tri_rec y z =
 		match y with
@@ -143,10 +65,6 @@ let tri_topologique g =
 		 		(tri_rec yp zp)@[t]	
 	in tri_rec (sansDep g) [] 
 ;;
-
-(* tests *)
-let trig1 = tri_topologique g1;;
-let printtrig1 = printListeSommets trig1;;
 
 
 
@@ -174,7 +92,7 @@ let rec etape_ordonnanceur g y z res =
 		| t::q ->	
 			(* not enough resources *)
 			if (res < 1) then
-				(y@ytodo, z, result)
+				(ytodo@y, z, result)
 			else
 				(* enough *)
 				let zp = t::z in
@@ -193,11 +111,6 @@ let ordonnanceur_sans_heuristique res g =
 	in ordonnanceur_full (sansDep g) [] []
 ;;
 
-(* tests *)
-let ordog1 = ordonnanceur_sans_heuristique 3 g1;;
-let printordog1 = printListeListeSommets ordog1;;
-let ordog2 = ordonnanceur_sans_heuristique 3 g2;;
-let printordog2 = printListeListeSommets ordog2;;
 
 (* entrees: 
    - un nombre entier de ressources r
@@ -222,28 +135,14 @@ let rec length_chemin_critique g v =
 			1 + List.fold_left (fun acc vx -> max acc (length_chemin_critique g vx)) 0 listeSucc	
 ;;
 
-(* tests *)
-let lengthg1a = length_chemin_critique g1 ag1;;
-let lengthg2a = length_chemin_critique g2 ag2;;
-let lengthg2b = length_chemin_critique g2 bg2;;
-let lengthg2c = length_chemin_critique g2 cg2;;
-
-
 let sortBy_chemin_critique g l =
-	List.sort (fun a b -> if ((Mark.get a)  < (Mark.get b)) then -1 else (if ((Mark.get a) > (Mark.get b)) then 1 else 0)) l
+	List.sort (fun a b -> if ((Mark.get a) > (Mark.get b)) then -1 else (if ((Mark.get a) < (Mark.get b)) then 1 else 0)) l
 ;;
 
 let markBy_chemin_critique g =
 	fold_vertex (fun vt vq -> Mark.set vt (length_chemin_critique g vt)) g ()
 ;;
 
-(* tests *)
-let test_chemin_critique_mark = begin (markBy_chemin_critique g1); printSommetsMark g1 end ;;
-
-(* tests *)
-let sortedg2= sortBy_chemin_critique g2 [ag2;bg2;cg2];;
-let printBeforeSort = printListeSommets [ag2;bg2;cg2];;
-let printAfterSort = printListeSommets sortedg2;;
 
 let ordonnanceur_avec_heuristique res g =
 	begin
@@ -252,17 +151,11 @@ let ordonnanceur_avec_heuristique res g =
 			match y with
 			|[] -> result
 			|t::q ->
-				let (y_etape, z_etape, result_etape) = etape_ordonnanceur g y z res 
-				in ordonnanceur_full (sortBy_chemin_critique g y_etape) z_etape (result@[result_etape])
+				let (y_etape, z_etape, result_etape) = etape_ordonnanceur g (sortBy_chemin_critique g y) z res 
+				in ordonnanceur_full y_etape z_etape (result@[result_etape])
 		in ordonnanceur_full (sansDep g) [] []
 	end	
 ;;
-
-(* tests *)
-let ordog1h = ordonnanceur_avec_heuristique 3 g1;;
-let printordog1h = printListeListeSommets ordog1h;;
-let ordog2h = ordonnanceur_avec_heuristique 3 g2;;
-let printordog2h = printListeListeSommets ordog2h;;
 
 
 (* entrees: 
@@ -278,7 +171,6 @@ let printordog2h = printListeListeSommets ordog2h;;
 val ordonnanceur_contrainte_memoire : int -> int -> DAG.t -> trace
    *)
 
-
 let rec etape_ordonnanceur_contrainte g y z res mem =
 	let rec tri_rec y ytodo z result res mact =
 		match y with
@@ -286,7 +178,7 @@ let rec etape_ordonnanceur_contrainte g y z res mem =
 		| t::q ->	
 			(* not enough resources or memory *)
 			if ((res < 1) || (mem < mact)) then
-				(y@ytodo, z, result)
+				(ytodo@y, z, result)
 			else
 				(* enough *)
 				let zp = t::z in
@@ -304,11 +196,10 @@ let ordonnanceur_contrainte_memoire res mem g =
 			|[] -> result
 			|t::q ->
 				let (y_etape, z_etape, result_etape) = etape_ordonnanceur_contrainte g y z res mem
-				in ordonnanceur_full (sortBy_chemin_critique g y_etape) z_etape (result@[result_etape])
+				in ordonnanceur_full (sortBy_chemin_critique_mem g y_etape) z_etape (result@[result_etape])
 		in ordonnanceur_full (sansDep g) [] []	
 	end
 ;;
-
 
 
 (* entrees: 
@@ -323,8 +214,53 @@ let ordonnanceur_contrainte_memoire res mem g =
    - vous utiliserez une heuristique specifique au cas contraint
 val ordonnanceur_contrainte_memoire_bonus : int -> int -> DAG.t -> trace
    *)
+(* heuristique : à chaque sous étape on execute le plus grand nombre de tâches possible en remplissant au max la mémoire *)
+
+let sortBy_chemin_critique_mem g l = 
+	let rec tri_iter l =  
+		match l with
+		|[] -> l
+		|t1::t2::q ->
+			if ((Mark.get t1) = (Mark.get t2)) then 
+				if ((DAG.Attributes.vertex_mem t1) <= (DAG.Attributes.vertex_mem t2)) then 
+					t1::t2::q					
+				else
+					t2::t1::q
+			else
+				t1::t2::q
+	in tri_iter (sortBy_chemin_critique g l) 
+;; 
 
 
+let rec etape_ordonnanceur_contrainte_bonus g y z res mem =
+	let rec tri_rec y ytodo z result res mact =
+		match y with
+		| [] -> (ytodo, z ,result)
+		| t::q ->	
+			(* not enough resources or memory *)
+			if ((res < 1) || (mem < mact)) then
+				(ytodo@y, z, result)
+			else
+				(* enough *)
+				let zp = t::z in
+					let yp = fold_succ (fun vt vq -> if (listInclude (pred g vt) zp) then vq@[vt] else vq) g t ytodo in	
+ 					(tri_rec q yp zp (result@[t]) (res - 1) (mact + (DAG.Attributes.vertex_mem t)))	
+	in tri_rec y [] z [] res 0
+;;
+
+
+let ordonnanceur_contrainte_memoire_bonus res mem g =
+	begin
+		markBy_chemin_critique g;
+		let rec ordonnanceur_full y z result =
+			match y with
+			|[] -> result
+			|t::q ->
+				let (y_etape, z_etape, result_etape) = etape_ordonnanceur_contrainte_bonus g y z res mem
+				in ordonnanceur_full (sortBy_chemin_critique g y_etape) z_etape (result@[result_etape])
+		in ordonnanceur_full (sansDep g) [] []	
+	end
+;;
 
 
 
